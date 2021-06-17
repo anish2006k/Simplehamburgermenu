8b52e61a17ce52fbf3d97c18a1d7250a854f1ff3
anish2006k <75366614+anish2006k@users.noreply.github.com>
2021-06-17 15:44:44 +0530

    Add files via upload

---
+++ toggle menu/script.js
@@ -0,0 +1,8 @@
+function toggle() {
+  var nav = document.getElementById("navbar");
+  if (nav.style.display === "none") {
+    nav.style.display = "block";
+  } else {
+    nav.style.display = "none";
+  }
+}
